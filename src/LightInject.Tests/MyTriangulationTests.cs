902beb47384b8dd8db583d86519f614309633f67
Kenny Pflug <kenny.pflug@live.de>
2016-11-08 15:35:43 +0100

    added MultipleResolveWithScopedLifetime

---
+++ src/LightInject.Tests/MyTriangulationTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LightInject.Tests
+{
+    public sealed class MyTriangulationTests
+    {
+        [Fact]
+        public void MultipleResolveWithScopedLifetime()
+        {
+            var container = new ServiceContainer();
+            container.Register<IFoo, FooA>()
+                     .Register<IBar, BarA>(new PerScopeLifetime())
+                     .Register<Baz>();
+
+            using (container.BeginScope())
+            {
+                var first = container.GetInstance<Baz>();
+                var second = container.GetInstance<Baz>();
+
+                first.Should().NotBeSameAs(second);
+                first.Bar.Should().BeSameAs(second.Bar);
+            }
+        }
+
+        public interface IFoo { }
+
+        public class FooA : IFoo { }
+
+        public class FooB : IFoo { }
+
+        public interface IBar { }
+
+        public class BarA : IBar
+        {
+            public readonly IFoo Foo;
+
+            public BarA(IFoo foo)
+            {
+                Foo = foo;
+            }
+        }
+
+        public interface IBaz { }
+
+        public class Baz : IBaz
+        {
+            public readonly IBar Bar;
+
+            public Baz(IBar bar)
+            {
+                Bar = bar;
+            }
+        }
+    }
+}
